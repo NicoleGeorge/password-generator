57794bf3c2915366d81147f91d76a5df5cbe475e
Nicole George <hello@nicolegeorge.me>
2020-03-07 17:20:23 +1030

    fixed file, wrong html uploaded. Updated files not correct. Working from 2 computers-upload mistake

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
+// Syncing the Slider & Number input fields
+
 var charRange = document.getElementById("charRange")
 var charNumber = document.getElementById("charNumber")
-var passwordGeneratorForm = document.getElementById("passwordGeneratorForm")
-var includeLowercaseEl = document.getElementById("includeLowerCase")
-var includeUppercaseEl = document.getElementById("includeUpperCase")
-var includeNumbersEl = document.getElementById("includeNumbers")
-var includeSymbolsEl = document.getElementById("includeSymbols")
 
-// Stage 1: Syncing the Slider & Number input fields - making them dynamic
 charRange.addEventListener("input", syncCharAmount)
 charNumber.addEventListener("input", syncCharAmount)
-
-// Stage 2: Syncing the checkboxes to the password generate field
-
-FormData.addEventListener("generate", event => ) {
-    event.preventDefault()
-    var CharacterAmount = charNumber.value
-    var includeLowercase = includeLowercaseEl.checked
-    var includeUppercaseEl = includeUppercase.checked
-    var includeNumbersEl = includeNumbers.checked
-    var includeSymbolsEl = includeSymbols.checked
-    var password = generatePassword(CharacterAmount, includeLowercase, includeUppercase, includeNumbers, includeSymbol); 
-}
 
 function syncCharAmount (event) {
   var value = event.target.value
   charNumber.value = value
   charRange.value = value
 }
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
